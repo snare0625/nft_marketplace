1d3769afe73f3415f900447e0e0a329e980a2d58
Neo Sekhukhune <sekhukhune056@gmail.com>
2024-08-30 20:09:32 +0200

    Completed Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,15 @@ import { useTheme } from 'next-themes';
 import images from '../assets';
 import { Button } from './index.js';
 
+const FooterLinks = ({ heading, items }) => (
+  <div className="flex-1 justify-start items-start">
+    <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-lg mb-10">{heading}</h3>
+    {items.map((item, index) => (
+      <p key={index} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">{item}</p>
+    ))}
+  </div>
+);
+
 const Footer = () => {
   const { theme } = useTheme();
 
@@ -16,9 +25,17 @@ const Footer = () => {
             <p className="dark:text-white text-nft-black-1 font-semibold text-lg p-1">NeoExchange</p>
           </div>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">Get the latest updates</p>
-          <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6">
-            sd
+          <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
+            <input type="email" placeholder="Your Email" className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none" />
+            <div className="flex-initial">
+              <Button btnName="Email me" classStyles="rounded-md" />
+            </div>
           </div>
+        </div>
+
+        <div className="flex-1 flexBetweenStart flex-wrap m1-10 md:ml-0 md:mt-8">
+          <FooterLinks heading="NeoExchange" items={['Explore', 'How it Works', 'Contact Us']} />
+          <FooterLinks heading="Support" items={['Help Center', 'Terms of Service', 'Legal', 'Privacy Policy']} />
         </div>
       </div>
       <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
@@ -34,7 +51,7 @@ const Footer = () => {
                     width={24}
                     height={24}
                     alt="social"
-                    className={theme === 'ligh t' && 'filter invert'}
+                    className={theme === 'light' && 'filter invert'}
                   />
                 </div>
               ),
