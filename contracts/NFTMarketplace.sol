408d340d642c6545891055181b5e9df426c5efb3
Neo Sekhukhune <sekhukhune056@gmail.com>
2024-11-25 20:12:12 +0200

    Added createMarketItem function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -65,10 +65,49 @@ contract NFTMarketPlace is ERC721URIStorage {
         listingPrice = _listingPrice;
     }
 
+    /* Returns the listing price of the contract */
+    // when we deploy the contract on the frontend we don't know how much to list it for
+    // so we call the contract and get the listing price and make sure we're sending the right amount
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
 
-    
+    /* Mints a token and lists it in the marketplace */
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();
+        // variable that get's the value of the tokenIds (0, 1, 2..)
+        uint256 newTokenId = _tokenIds.current();
+        // mint the token with
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        createMarketItem(newTokenId, price);
+        // we've just minted the token and make it sellable
 
+        // now we can return the id to the client side so we can work with it
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        // require a certain CONDITION, in this case price greater than 0
+        require(price > 0, "Price must be at least 1 wei");
+        // require that the users sending in the transaction is sending in the correct amount
+        require(msg.value == listingPrice, "Price must be equal to listingn price");
+
+        // create the mapping for the market items
+        // payable(address(0))n is the owner
+        // currently there's no owner as the seller is putting it to market so it's an empty address
+        // last value is boolean for sold, its false because we just put it so it's not sold yet
+        // this is creating the first market item
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        // transfer the ownership of the nft to the contract -> next buyer
+        _transfer(msg.sender, address(this), tokenId);
+        emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
+    }
 }
