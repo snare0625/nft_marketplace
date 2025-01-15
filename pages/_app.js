3e9ae8ec669f7800f703fccc153db284fa717ffb
Neo Sekhukhune <sekhukhune056@gmail.com>
2025-01-15 15:19:59 +0200

    Added NFT Context Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,25 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { NFTProvider } from '../context/NFTContext';
 import '../styles/globals.css';
 import { Navbar, Footer } from '../components';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/852e6c0ef5.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/852e6c0ef5.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
+
 );
 
 export default MyApp;
